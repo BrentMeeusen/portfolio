c6e17c9d97d6e38b618dd4e6331f3e1e952bfb9e
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-07-10 15:08:15 +0200

    Added code for making the menu interactive

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,5 +1,22 @@
+const menu = {
+	init: () => {
+		console.log(this, "joemama");
+	},
+	open: () => {
+
+	}
+}
+
+
+
+
+
 window.addEventListener("load", () => {
 
+	// Initialise menu
+	menu.init();
+
+	// Set click event for opening the menu
 	document.getElementById("open-menu").addEventListener("click", () => {
 		menu.open();
 	});
